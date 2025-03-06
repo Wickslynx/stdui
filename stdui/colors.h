113aed8496066f0e04c3c9f97be789be2c6335c3
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-06 18:28:05 +0100

    Fix.

---
+++ stdui/colors.h
@@ -0,0 +1,129 @@
+// Basic Colors
+float red[3] = {1.0f, 0.0f, 0.0f};
+float green[3] = {0.0f, 1.0f, 0.0f};
+float blue[3] = {0.0f, 0.0f, 1.0f};
+float yellow[3] = {1.0f, 1.0f, 0.0f};
+float cyan[3] = {0.0f, 1.0f, 1.0f};
+float magenta[3] = {1.0f, 0.0f, 1.0f};
+float black[3] = {0.0f, 0.0f, 0.0f};
+float white[3] = {1.0f, 1.0f, 1.0f};
+float gray[3] = {0.5f, 0.5f, 0.5f};
+
+// Reds
+float darkRed[3] = {0.545f, 0.0f, 0.0f};
+float firebrick[3] = {0.698f, 0.133f, 0.133f};
+float indianRed[3] = {0.804f, 0.361f, 0.361f};
+float lightCoral[3] = {0.941f, 0.502f, 0.502f};
+float salmon[3] = {0.980f, 0.502f, 0.447f};
+float crimson[3] = {0.863f, 0.078f, 0.235f};
+float tomato[3] = {1.0f, 0.388f, 0.278f};
+float coral[3] = {1.0f, 0.498f, 0.314f};
+
+// Oranges
+float orangeRed[3] = {1.0f, 0.271f, 0.0f};
+float darkOrange[3] = {1.0f, 0.549f, 0.0f};
+float orange[3] = {1.0f, 0.647f, 0.0f};
+
+// Yellows
+float gold[3] = {1.0f, 0.843f, 0.0f};
+float goldenrod[3] = {0.855f, 0.647f, 0.125f};
+float darkGoldenrod[3] = {0.722f, 0.525f, 0.043f};
+float khaki[3] = {0.941f, 0.902f, 0.549f};
+
+// Greens
+float darkGreen[3] = {0.0f, 0.392f, 0.0f};
+float forestGreen[3] = {0.133f, 0.545f, 0.133f};
+float seaGreen[3] = {0.180f, 0.545f, 0.341f};
+float olive[3] = {0.502f, 0.502f, 0.0f};
+float oliveDrab[3] = {0.420f, 0.557f, 0.137f};
+float limeGreen[3] = {0.196f, 0.804f, 0.196f};
+float lime[3] = {0.0f, 1.0f, 0.0f};
+float springGreen[3] = {0.0f, 1.0f, 0.498f};
+float mediumSpringGreen[3] = {0.0f, 0.980f, 0.604f};
+float paleGreen[3] = {0.596f, 0.984f, 0.596f};
+float lightGreen[3] = {0.565f, 0.933f, 0.565f};
+float greenYellow[3] = {0.678f, 1.0f, 0.184f};
+float chartreuse[3] = {0.498f, 1.0f, 0.0f};
+float lawnGreen[3] = {0.486f, 0.988f, 0.0f};
+
+// Cyans
+float teal[3] = {0.0f, 0.502f, 0.502f};
+float darkCyan[3] = {0.0f, 0.545f, 0.545f};
+float lightSeaGreen[3] = {0.125f, 0.698f, 0.667f};
+float turquoise[3] = {0.251f, 0.878f, 0.816f};
+float aquamarine[3] = {0.498f, 1.0f, 0.831f};
+float aqua[3] = {0.0f, 1.0f, 1.0f};
+float paleturquoise[3] = {0.686f, 0.933f, 0.933f};
+
+// Blues
+float navyBlue[3] = {0.0f, 0.0f, 0.502f};
+float darkBlue[3] = {0.0f, 0.0f, 0.545f};
+float mediumBlue[3] = {0.0f, 0.0f, 0.804f};
+float royalBlue[3] = {0.255f, 0.412f, 0.882f};
+float dodgerBlue[3] = {0.118f, 0.565f, 1.0f};
+float deepSkyBlue[3] = {0.0f, 0.749f, 1.0f};
+float skyBlue[3] = {0.529f, 0.808f, 0.922f};
+float lightSkyBlue[3] = {0.529f, 0.808f, 0.980f};
+float steelBlue[3] = {0.275f, 0.510f, 0.706f};
+float lightSteelBlue[3] = {0.690f, 0.769f, 0.871f};
+float lightBlue[3] = {0.678f, 0.847f, 0.902f};
+float powderBlue[3] = {0.690f, 0.878f, 0.902f};
+
+// Purples
+float indigo[3] = {0.294f, 0.0f, 0.510f};
+float purple[3] = {0.502f, 0.0f, 0.502f};
+float darkMagenta[3] = {0.545f, 0.0f, 0.545f};
+float darkViolet[3] = {0.580f, 0.0f, 0.827f};
+float darkOrchid[3] = {0.600f, 0.196f, 0.800f};
+float mediumOrchid[3] = {0.729f, 0.333f, 0.827f};
+float thistle[3] = {0.847f, 0.749f, 0.847f};
+float plum[3] = {0.867f, 0.627f, 0.867f};
+float violet[3] = {0.933f, 0.510f, 0.933f};
+float orchid[3] = {0.855f, 0.439f, 0.839f};
+float mediumPurple[3] = {0.576f, 0.439f, 0.859f};
+float slateBlue[3] = {0.416f, 0.353f, 0.804f};
+float mediumSlateBlue[3] = {0.482f, 0.408f, 0.933f};
+float blueViolet[3] = {0.541f, 0.169f, 0.886f};
+
+// Browns
+float cornsilk[3] = {1.0f, 0.973f, 0.863f};
+float blanchedAlmond[3] = {1.0f, 0.922f, 0.804f};
+float bisque[3] = {1.0f, 0.894f, 0.769f};
+float navajoWhite[3] = {1.0f, 0.871f, 0.678f};
+float wheat[3] = {0.961f, 0.871f, 0.702f};
+float burlyWood[3] = {0.871f, 0.722f, 0.529f};
+float tan[3] = {0.824f, 0.706f, 0.549f};
+float rosyBrown[3] = {0.737f, 0.561f, 0.561f};
+float sandyBrown[3] = {0.957f, 0.643f, 0.376f};
+float peru[3] = {0.804f, 0.522f, 0.247f};
+float chocolate[3] = {0.824f, 0.412f, 0.118f};
+float saddleBrown[3] = {0.545f, 0.271f, 0.075f};
+float sienna[3] = {0.627f, 0.322f, 0.176f};
+float brown[3] = {0.647f, 0.165f, 0.165f};
+float maroon[3] = {0.502f, 0.0f, 0.0f};
+
+// Whites/Grays/Blacks
+float snow[3] = {1.0f, 0.980f, 0.980f};
+float honeydew[3] = {0.941f, 1.0f, 0.941f};
+float mintCream[3] = {0.961f, 1.0f, 0.980f};
+float azure[3] = {0.941f, 1.0f, 1.0f};
+float aliceBlue[3] = {0.941f, 0.973f, 1.0f};
+float ghostWhite[3] = {0.973f, 0.973f, 1.0f};
+float whiteSmoke[3] = {0.961f, 0.961f, 0.961f};
+float seashell[3] = {1.0f, 0.961f, 0.933f};
+float beige[3] = {0.961f, 0.961f, 0.863f};
+float oldLace[3] = {0.992f, 0.961f, 0.902f};
+float floralWhite[3] = {1.0f, 0.980f, 0.941f};
+float ivory[3] = {1.0f, 1.0f, 0.941f};
+float antiqueWhite[3] = {0.980f, 0.922f, 0.843f};
+float linen[3] = {0.980f, 0.941f, 0.902f};
+float lavenderBlush[3] = {1.0f, 0.941f, 0.961f};
+float mistyRose[3] = {1.0f, 0.894f, 0.882f};
+float gainsboro[3] = {0.863f, 0.863f, 0.863f};
+float lightGray[3] = {0.827f, 0.827f, 0.827f};
+float silver[3] = {0.753f, 0.753f, 0.753f};
+float darkGray[3] = {0.663f, 0.663f, 0.663f};
+float dimGray[3] = {0.412f, 0.412f, 0.412f};
+float lightSlateGray[3] = {0.467f, 0.533f, 0.6f};
+float slateGray[3] = {0.439f, 0.502f, 0.565f};
+float darkSlateGray[3] = {0.184f, 0.310f, 0.310f};
--- stdui/internal/utils.h
+++
@@ -1,5 +0,0 @@
-typedef struct {
-  float red[3];   
-  float green[3]; 
-  float blue[3];  
-} color;
