2dfd5263b9462129f8c9a0e513038ac841437c7b
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-06 18:15:51 +0100

    Fix.

--- stdui/widgets.h
+++ stdui/widgets.h
@@ -1,33 +1,32 @@
 #ifndef WIDGETS_H
 #define WIDGETS_H
-
 #include <stdio.h>
-
 #include "internal/utils.h"
-
 #if defined(GL_VERSION)
 #include <GL/gl.h>
-
 #if defined(__linux__)
 #include <GL/glx.h>
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #elif defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
+#include <GL/gl.h>
 #endif
-
 
 void STriangle(SApplication *app, float color[3]) {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glBegin(GL_TRIANGLES);
         glColor3f(color[0], color[1], color[2]);
         glVertex2f(-0.5f, -0.5f); 
         glVertex2f(0.5f, -0.5f);  
         glVertex2f(0.0f, 0.5f);   
     glEnd();
-
+    
+    #if defined(__linux__)
     glXSwapBuffers(app->display, app->window);
+    #elif defined(_WIN32) || defined(_WIN64)
+    SwapBuffers(app->hdc);
+    #endif
 }
 
 void SRectangle(SApplication *app, float color[3]) {
@@ -38,8 +37,12 @@ void SRectangle(SApplication *app, float color[3]) {
         glVertex2f(0.5f, 0.5f);   
         glVertex2f(-0.5f, 0.5f);  
     glEnd();
-
+    
+    #if defined(__linux__)
     glXSwapBuffers(app->display, app->window);
+    #elif defined(_WIN32) || defined(_WIN64)
+    SwapBuffers(app->hdc);
+    #endif
 }
 
 void SCircle(SApplication *app, float *color) {
@@ -48,9 +51,6 @@ void SCircle(SApplication *app, float *color) {
 }
 
 #elif defined(VULKAN_VERSION_1_0)
-
 #else 
-
 #endif //VULKAN
-
 #endif //WIDGETS_H
