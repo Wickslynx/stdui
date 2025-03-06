fe4d0e37740719d72ff8cb6f0570d829f7b1bf51
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-03-06 12:08:36 +0100

    Fix.

--- stdui/internal/utils.h
+++ stdui/internal/utils.h
@@ -1,3 +1,6 @@
-#define RED static float red[3] = {1.0f, 0.0f, 0.0f};
-#define GREEN static float green[3] = {0.0f, 1.0f, 0.0f};
-#define BLUE static float blue[3] = {0.0f, 0.0f, 1.0f};
+typedef struct {
+  red = {1.0f, 0.0f, 0.0f};
+  green = {0.0f, 1.0f, 0.0f};
+  blue = {0.0f, 0.0f, 1.0f};
+  
+} color;
